432ffeba8ba1d49becce691903ac71a85ee9d8c0
Conor <28988626+Snaddyvitch-Dispenser@users.noreply.github.com>
2020-05-07 14:54:39 +0100

    Update PHP code

--- SpeedyBeesMiddleware - Prevent users from getting challenge answers/challenge.php
+++ SpeedyBeesMiddleware - Prevent users from getting challenge answers/challenge.php
@@ -1,101 +1,83 @@
 <?php
-$currentCookieParams = session_get_cookie_params();
 
-$rootDomain = '.dmessages.app';
-$expiresAt = time() + 604800;
-$secureCookie = true;
-
-session_set_cookie_params(
-    $expiresAt,
-    "/",
-    $rootDomain,
-    $secureCookie
-);
-
-session_name('APISESSION');
-session_start();
-
-setcookie('APISESSION', session_id(), $expiresAt, '/', $rootDomain);
-
-// Fix CORS
-if (isset($_SERVER['HTTP_ORIGIN'])) {
-    // Decide if the origin in $_SERVER['HTTP_ORIGIN'] is one
-    // you want to allow, and if so:
-    $host_names = explode(".", $_SERVER["HTTP_ORIGIN"]);
-    if ($host_names[count($host_names)-2] . "." . $host_names[count($host_names)-1] == "dmessages.app") {
-        header("Access-Control-Allow-Origin: $_SERVER[HTTP_ORIGIN]");
-        header('Access-Control-Allow-Credentials: true');
-        header('Access-Control-Max-Age: 0');    // cache for 1 day
-    }
-}
+// Import session & cors logic - Bonus - $config is now available in this document
+require "sessions.php";
 
 if (isset($_GET["user"])) {
-    if (isset($_SESSION["challenge_answer"]) and isset($_SESSION["challenge_question"]) and isset($_SESSION["challenge_user"]) and $_SESSION["challenge_user"] == $_GET["user"]) {
-        header("Content-Type: application/json");
-        $data = [
-            "success" => true,
-            "data" => [
-                "challenge" => $_SESSION["challenge_question"],
-                "user" => $_SESSION["challenge_user"]
-            ]
-        ];
-        echo(json_encode($data));
-    } else {
-        // Open a connection and get a challenge and answer for the current user
-        $curl = curl_init();
-
-        curl_setopt_array($curl, array(
-            CURLOPT_URL => "https://api.steem.tools/get_message",
-            CURLOPT_RETURNTRANSFER => true,
-            CURLOPT_ENCODING => "",
-            CURLOPT_MAXREDIRS => 10,
-            CURLOPT_TIMEOUT => 30,
-            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-            CURLOPT_CUSTOMREQUEST => "POST",
-            CURLOPT_POSTFIELDS => "name=" . $_GET["user"],
-            CURLOPT_HTTPHEADER => array(
-                "Cache-Control: no-cache",
-                "Content-Type: application/x-www-form-urlencoded"
-            ),
-        ));
-
-        // Get any responses and errors
-        $response = curl_exec($curl);
-        $err = curl_error($curl);
-
-        // Close connection
-        curl_close($curl);
-
-        // Check for web server errors
-        if ($err) {
+    if (!isset($_SESSION["username"]) or $_SESSION["username"] != $_GET["user"]) {
+        if (isset($_SESSION["challenge_answer"]) and isset($_SESSION["challenge_question"]) and isset($_SESSION["challenge_user"]) and $_SESSION["challenge_user"] == $_GET["user"]) {
             header("Content-Type: application/json");
-            print('{"success": false, "error_message": "Error signing challenge, please try again later!"}');
-            die();
+            $data = [
+                "success" => true,
+                "data" => [
+                    "challenge" => $_SESSION["challenge_question"],
+                    "user" => $_SESSION["challenge_user"]
+                ]
+            ];
+            echo(json_encode($data));
         } else {
-            // Decode JSON
-            $json = json_decode($response);
-            // If it succeeded
-            if ($json->success) {
-                // Save in the session, in case the same user requests another challenge before logging in (prevents "invalid challenge" if open in multiple tabs)
-                $_SESSION["challenge_answer"] = $json->data->answer;
-                $_SESSION["challenge_question"] = $json->data->challenge;
-                $_SESSION["challenge_user"] = $json->data->user;
-                // Repeat only the challenge back to the user, for them to solve and answer
+            // Open a connection and get a challenge and answer for the current user
+            $curl = curl_init();
+
+            curl_setopt_array($curl, array(
+                CURLOPT_URL => "https://api.steem.tools/get_message",
+                CURLOPT_RETURNTRANSFER => true,
+                CURLOPT_ENCODING => "",
+                CURLOPT_MAXREDIRS => 10,
+                CURLOPT_TIMEOUT => 30,
+                CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+                CURLOPT_CUSTOMREQUEST => "POST",
+                CURLOPT_POSTFIELDS => "name=" . strtolower($_GET["user"]) . "&app=" . $config->app->name . "&author=" . $config->app->author,
+                CURLOPT_HTTPHEADER => array(
+                    "Cache-Control: no-cache",
+                    "Content-Type: application/x-www-form-urlencoded"
+                ),
+            ));
+
+            // Get any responses and errors
+            $response = curl_exec($curl);
+            $err = curl_error($curl);
+
+            // Close connection
+            curl_close($curl);
+
+            // Check for web server errors
+            if ($err) {
                 header("Content-Type: application/json");
-                $data = [
-                    "success" => true,
-                    "data" => [
-                        "challenge" => $_SESSION["challenge_question"],
-                        "user" => $_SESSION["challenge_user"]
-                    ]
-                ];
-                echo(json_encode($data));
+                print('{"success": false, "error_message": "Error signing challenge, please try again later!"}');
+                die();
             } else {
-                // Otherwise report an error and pass through the error.
-                header("Content-Type: application/json");
-                print('{"success": false, "error_message": "' . $json->error_message . '"}');
+                // Decode JSON
+                $json = json_decode($response);
+                // If it succeeded
+                if ($json->success) {
+                    // Save in the session, in case the same user requests another challenge before logging in (prevents "invalid challenge" if open in multiple tabs)
+                    $_SESSION["challenge_answer"] = $json->data->answer;
+                    $_SESSION["challenge_question"] = $json->data->challenge;
+                    $_SESSION["challenge_user"] = $json->data->user;
+                    // Repeat only the challenge back to the user, for them to solve and answer
+                    header("Content-Type: application/json");
+                    $data = [
+                        "success" => true,
+                        "data" => [
+                            "challenge" => $_SESSION["challenge_question"],
+                            "user" => $_SESSION["challenge_user"]
+                        ]
+                    ];
+                    echo(json_encode($data));
+                } else {
+                    // Otherwise report an error and pass through the error.
+                    header("Content-Type: application/json");
+                    print('{"success": false, "error_message": "' . $json->error_message . '"}');
+                }
             }
         }
+    } else {
+        json_return([
+            "success" => false,
+            "error_message" => "You're already signed in. Please reload the page.",
+            "user" => $_SESSION["username"]
+        ]);
     }
 } else {
     // No user specified
---
+++ SpeedyBeesMiddleware - Prevent users from getting challenge answers/sessions.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Conor Howland
+ * Date: 21/04/2020
+ * Time: 22:43
+ */
+
+$config = require "config.php";
+
+// Session Setup
+
+// Set Parameters from config file
+session_set_cookie_params(
+    $config->cookies->session->expiry,
+    $config->cookies->path,
+    $config->cookies->domain,
+    $config->cookies->session->secure
+);
+
+// Set session name and start session
+session_name($config->cookies->session->name);
+session_start();
+
+// Set session cookie with correct timings
+setcookie($config->cookies->session->name, session_id(), $config->cookies->session->expiry, $config->cookies->path, $config->cookies->domain);
+
+// Due to the fact that we are using multiple sub-domains, we must enable Cross-Origin-Resource-Sharing (CORS) for our entire domain.
+if (isset($_SERVER['HTTP_ORIGIN'])) {
+    // Split Host Name into portions
+    $host_names = explode(".", $_SERVER["HTTP_ORIGIN"]);
+    $host_domain = $host_names[count($host_names)-2] . "." . $host_names[count($host_names)-1];
+
+    // Get domain and check if it's ours
+    if ($host_domain === $config->app->domain or $host_domain === "localhost" or $host_domain === "localhost:3000") {
+        // Set CORS headers so browsers can access this page.
+        header("Access-Control-Allow-Origin: $_SERVER[HTTP_ORIGIN]");
+        header('Access-Control-Allow-Credentials: true');
+        header('Access-Control-Max-Age: 0');    // cache for 1 day
+        header('Access-Control-Allow-Methods: GET,PUT,POST,DELETE,PATCH,OPTIONS'); // Allow all kinds of requests
+        header("Access-Control-Allow-Headers: Origin,X-Requested-With,Content-Type,Accept,Authorisation");
+    }
+}
+
+// API Return Values
+function json_return($array = []) {
+    header("Content-Type: application/json");
+    print(json_encode($array));
+}
